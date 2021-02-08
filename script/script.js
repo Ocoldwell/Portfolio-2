a506590923d9ada0e69dbce96423ebeb07c21a0b
Oliver Coldwell <olivercoldwell@gmail.com>
2021-02-08 08:32:44 +0000

    added typing animation in about me

---
+++ script/script.js
@@ -0,0 +1,42 @@
+// set up text to print, each item in array is new line
+const aText = new Array(
+  "Hi, I'm Ollie, I'm a Web Developer.", 
+  "About me? I love dogs, clean code, and fantasy books.",
+  "In that order.",
+  "I worked in retail for 13 years, I know how to deliver to customers.",
+  "I enjoy leading and solving problems in a team.",
+  "Check out some of my recent projects below."
+  );
+  let iSpeed = 100; // time delay of print out
+  let iIndex = 0; // start printing array at this posision
+  let iArrLength = aText[0].length; // the length of the text array
+  let iScrollAt = 20; // start scrolling up at this many lines
+   
+  let iTextPos = 0; // initialise text position
+  let sContents = ''; // initialise contents variable
+  let iRow; // initialise current row
+   
+  function typewriter()
+  {
+   sContents =  ' ';
+   iRow = Math.max(0, iIndex-iScrollAt);
+   let destination = document.getElementById("typedText");
+   
+   while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
+   }
+   destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+   if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+    if ( iIndex != aText.length ) {
+     iArrLength = aText[iIndex].length;
+     setTimeout("typewriter()", 500);
+    }
+   } else {
+    setTimeout("typewriter()", iSpeed);
+   }
+  }
+  
+  
+  typewriter();
